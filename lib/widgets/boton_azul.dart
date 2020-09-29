5df5683744c68e394077f970418e686576939403
Jose Erasmo Solano Gonzalez <Erasmo@MacBook-Pro-de-Jose.local>
2020-09-29 13:29:22 -0600

    Login y registro terminado

---
+++ lib/widgets/boton_azul.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class BotonAzul extends StatelessWidget {
+  final String texto;
+  final Function onPressed;
+
+  const BotonAzul({
+    Key key,
+    @required this.texto,
+    @required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: this.onPressed,
+      elevation: 2,
+      highlightElevation: 5,
+      color: Colors.blue,
+      shape: StadiumBorder(),
+      child: Container(
+        width: double.infinity,
+        height: 55,
+        child: Center(
+          child: Text(
+            this.texto,
+            style: TextStyle(color: Colors.white, fontSize: 17),
+          ),
+        ),
+      ),
+    );
+  }
+}
